9bd449f2d8cf1e692799af5a552ecdcfb645f47c
Matjaz Lipus <matjazl@gmail.com>
2015-11-13 23:53:25 +0100

    use await in same expression

--- src/providers/http-json.js
+++ src/providers/http-json.js
@@ -3,8 +3,7 @@ import HttpProvider from './http';
 export default class HttpJSONProvider extends HttpProvider {
 
   async fetch() {
-    let response = await super.fetch();
-    return JSON.parse(response);
+    return JSON.parse(await super.fetch());
   }
 
 }
