6e728a1c5adc38f03918ceebf0d1612af2513d79
Matjaz Lipus <matjazl@gmail.com>
2022-06-15 00:54:59 +0200

    fb basic post & image

---
+++ src/parsers/fbBasicImage.js
@@ -0,0 +1,30 @@
+import { ocr } from '../util'
+import FBBasicPostParser from './fbBasicPost'
+
+export default class FBBasicImageParser extends FBBasicPostParser {
+  async parse (data) {
+    const postData = await super.parse(data)
+    if (!postData) {
+      return
+    }
+    const dates = Object.keys(postData)
+    const date = dates[0]
+    const postDayData = postData[date]
+    const imageUrl = postDayData && postDayData.offersImages && postDayData.offersImages[0]
+    if (!imageUrl) {
+      return
+    }
+    const ocrData = await ocr(imageUrl)
+    if (ocrData) {
+      const text = ocrData.ParsedResults[0] && ocrData.ParsedResults[0].ParsedText
+      return {
+        [date]: {
+          ...postDayData,
+          offers: [{
+            text,
+          }],
+        }
+      }
+    }
+  }
+}
--- src/parsers/fbBasicPost.js
+++ src/parsers/fbBasicPost.js
@@ -3,44 +3,86 @@ import cheerio from 'cheerio'
 
 import { toISODate } from '../util'
 
-const fetchPageHtml = async (linkPath) => {
-  const url = `https://mbasic.facebook.com${linkPath}`
-  return rp({
-    url,
+function req (options) {
+  return rp ({
+    ...options,
     headers: {
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
     }
   })
 }
 
-async function parsePhotoPage (linkPath) {
-  const html = await fetchPageHtml(linkPath)
+async function fetchPageHtml (linkPath, options) {
+  return req({
+    ...options,
+    url: `https://mbasic.facebook.com${linkPath}`,
+  })
+}
+
+async function confirmCookiePage (data, options) {
+  const $ = cheerio.load(data)
+  const form = $('form')
+  if (!form.length) {
+    return
+  }
+  let body = form.find('input').toArray().map(x => `${encodeURIComponent($(x).attr('name'))}=${encodeURIComponent($(x).attr('value'))}`).join('&')
+  body += '&accept_only_essential=1'
+
+  return req({
+    url: `https://mbasic.facebook.com${form.attr('action')}`,
+    method: form.attr('method'),
+    body,
+    ...options,
+  })
+}
+
+async function parsePhotoPage (linkPath, options) {
+  const html = await fetchPageHtml(linkPath, options)
   const $ = cheerio.load(html)
-  const image = $('#MPhotoContent div.desc.attachment > span > div > span > a[target=_blank].sec').attr('href')
+  const image = $('a[target].sec').attr('href')
   return image
 }
 
 export default class FBBasicPostParser {
+
+  jar = rp.jar()
+
   async parse (data) {
+    const { jar } = this
+    if (data.includes('/cookie/consent/')) {
+      data = await confirmCookiePage(data, {jar})
+    }
+    if (!data) {
+      return
+    }
     const $ = cheerio.load(data)
-    const $story = $('article[data-ft]').first()
-    const publishTime = FBBasicPostParser.articlePublishTime($story)
-    if (publishTime) {
-      const attachLinkList = $story.find('div > div > a').toArray().map(x => $(x).attr('href'))
-      const offersImages = await Promise.all(attachLinkList.map(link => parsePhotoPage(link)))
-      const date = toISODate(1000 * publishTime)
-      return {
-        [date]: {
-          offersImages
+    for (const el of $('article[data-ft]').toArray()) {
+      const $story = $(el)
+      const publishTime = FBBasicPostParser.articlePublishTime($story)
+      if (publishTime) {
+        var d = new Date(1000 * publishTime)
+        if (d.getHours() > 6 && d.getHours() < 12) {
+          const attachLinkList = $story.find('div > div > a').toArray().map(x => $(x).attr('href')).filter(x => x.includes('/photos/'))
+          const offersImages = await Promise.all(attachLinkList.map(link => parsePhotoPage(link, {jar})))
+          return {
+            [toISODate(d)]: {
+              offersImages
+            }
+          }
         }
       }
     }
   }
 
-  parseData (data) {
-    const $ = cheerio.load(data)
-    return {
-      name: $('#m-timeline-cover-section h1 span').text()
+  async parseData (data) {
+    if (data.includes('/cookie/consent/')) {
+      data = await confirmCookiePage(data, {jar: this.jar})
+    }
+    if (data) {
+      const $ = cheerio.load(data)
+      return {
+        name: $('#m-timeline-cover-section h1 span').text()
+      }
     }
   }
 
